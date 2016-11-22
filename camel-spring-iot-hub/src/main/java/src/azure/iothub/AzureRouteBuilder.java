9175245e5059f79e366f209121ec6f5b9f0e87ab
vivekprm <vivek.pr.mi@gmail.com>
2016-11-22 21:57:09 +0530

    Update AzureRouteBuilder.java

--- camel-spring-iot-hub/src/main/java/src/azure/iothub/AzureRouteBuilder.java
+++ camel-spring-iot-hub/src/main/java/src/azure/iothub/AzureRouteBuilder.java
@@ -9,22 +9,22 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
 
 /**
- * Created by cov-127 on 22/11/16.
+ * Created by vivek on 22/11/16.
  */
 @Component
 public class AzureRouteBuilder extends RouteBuilder {
     @Resource(name="UriTemplate")
     private String uriTemplate;
 
-    @Value("${rnd1.queue.name:iothub-ehub-ipf-demo-h-66245-f0d022c6b8/ConsumerGroups/$default/Partitions/0}")
-    private String queueRND1;
+    @Value("${queue.name:{}/ConsumerGroups/$default/Partitions/0}")
+    private String queue_1;
 
     @Autowired
     private ApplicationContext appContext;
 
     @Override
     public void configure() throws Exception {
-        from(uriTemplate.replace("$queueName", queueRND1)).streamCaching()
+        from(uriTemplate.replace("$queueName", queue_1)).streamCaching()
                 .process(appContext.getBean(AzureIOTHubProcessor.class));
     }
 }
